2074b47b06c50f60b09dd7f886aa9d49664dd8cd
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-03-22 21:10:27 +0600

    initial commit

---
+++ src/main/java/com/jr/cs/CsApplication.java
@@ -0,0 +1,13 @@
+package com.jr.cs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jr/cs/CsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jr.cs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
